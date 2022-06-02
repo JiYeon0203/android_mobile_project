d4389b9d541634af2a585072eb0db75c2504bf4a
parkjiyeon <gkdis310313@gmail.com>
2022-06-02 23:37:37 +0900

    06-02 update

--- app/src/main/java/com/baewha/androidmyproject/MainActivity.java
+++ app/src/main/java/com/baewha/androidmyproject/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnOkay, btnSignUp, btnIDPW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("건강검진 예약 어플");
 
+        btnOkay = findViewById(R.id.btnOkay);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnIDPW = findViewById(R.id.btnID_PW);
+
+        btnOkay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FirstActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
